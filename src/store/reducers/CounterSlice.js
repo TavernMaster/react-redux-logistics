c97aa3b8f8f2bbc926a0c0a4948e61f6ecb2ddd4
TavernMaster <dimasamusch@yandex.ru>
2023-05-29 19:45:00 +0300

    main

---
+++ src/store/reducers/CounterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0
+}
+
+const slice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        addCount(state, action) {
+            state.count += action.payload
+        },
+        reduceCount(state) {
+            state.count--
+        }
+    }
+})
+
+export default slice.reducer
+
+export const {addCount, reduceCount} = slice.actions
