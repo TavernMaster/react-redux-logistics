527240a09a5d0be0a679dfe1267f65af37aa3b3e
TavernMaster <dimasamusch@yandex.ru>
2023-05-30 17:21:54 +0300

    add router

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import AppRoutes from '../Routes/AppRoutes'
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<AppRoutes />
+			<Footer />
+		</div>
+	)
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import App from './components/App'
 import './styles/index.css'
 import {Provider} from 'react-redux'
 import store from './store/store'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</Provider>
 )
